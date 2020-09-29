bdc097c42967ef1f35f37e543ffceb4784b991a1
Richard Darst <rkd@zgib.net>
2020-09-29 18:55:52 +0300

    iterate, make validation succeed

--- validate.py
+++ validate.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
-import dateutil.parser
+import glob
 import json
+from os.path import dirname, join
 import sys
 
+import dateutil.parser
 from jsonschema import validate
 import yaml
 
-schema = yaml.safe_load(open(sys.argv[1]))
-data = yaml.safe_load(open(sys.argv[2]))
+SCHEMA = join(dirname(__file__), 'schema.yaml')
+ALL_COURSES = glob.glob(join(dirname(__file__), 'courses/*.yaml'))
 
-if dateutil.parser.parse(data['date']).date() > datetime.date.today():
-    schema['required'] += schema['required_post']
+parser = argparse.ArgumentParser()
+parser.add_argument('schema', nargs='?', help="schema file",
+                    default=SCHEMA)
+parser.add_argument('course', nargs='*', help="course files to process",
+                    default=ALL_COURSES)
+args = parser.parse_args()
+
+schema = yaml.safe_load(open(args.schema))
 
 
-validate(data, schema)
+for coursefile in args.course:
+    print
+    print(coursefile)
+    data = yaml.safe_load(open(coursefile))
 
+    if dateutil.parser.parse(data['date']).date() > datetime.date.today():
+        schema['required'] += schema['required_post']
+
+    validate(data, schema)
